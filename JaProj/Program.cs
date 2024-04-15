712e6290584c9dfc23882d12d1a7fa63130465c0
artur <mrarturno@gmail.com>
2024-04-15 17:50:57 +0200

    it works

--- JaProj/Program.cs
+++ JaProj/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,15 @@ namespace JaProj
 {
     internal class Program
     {
+        [DllImport(@"C:\Users\artur\source\repos\JaProj\x64\Debug\JAAsm.dll")]
+        static extern int MyProc1(int a, int b);
         static void Main(string[] args)
         {
+            int x = 5, y = 3;
+            int retVal = MyProc1(x, y);
+            Console.Write("Moja pierwsza wartosc obliczona w asm to: ");
+            Console.WriteLine(retVal);
+            Console.ReadLine();
         }
     }
 }
